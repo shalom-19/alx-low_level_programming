5a4daf6dc4600a7832fabe32fa16d7eaf46a1f6b
shalom-19 <113606094+shalom-19@users.noreply.github.com>
2022-10-18 15:57:03 +0100

    Update 0-putchar.c

--- 0-putchar.c
+++ 0-putchar.c
@@ -1,58 +1,18 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@shalom-19 
-shalom-19
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-alx-low_level_programming
-/
-0-putchar.c
-in
-main
- 
+#include "main.h"
+/**
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
+ */
+int main(void)
+{
+	char abe[] = "_putchar";
 
-Tabs
+	int c;
 
-8
-
-No wrap
-1
-​
-@shalom-19
-Commit new file
-Commit summary
-Create 0-putchar.c
-Optional extended description
-Add an optional extended description…
- Commit directly to the main branch.
- Create a new branch for this commit and start a pull request. Learn more about pull requests.
- 
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(abe[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
