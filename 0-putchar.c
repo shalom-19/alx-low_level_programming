0910c6dd6cf343f32e38856d42a13cc9f2307de3
shalom-19 <113606094+shalom-19@users.noreply.github.com>
2022-10-18 13:16:12 +0100

    Create 0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,58 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@shalom-19 
+shalom-19
+/
+alx-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+alx-low_level_programming
+/
+0-putchar.c
+in
+main
+ 
+
+Tabs
+
+8
+
+No wrap
+1
+​
+@shalom-19
+Commit new file
+Commit summary
+Create 0-putchar.c
+Optional extended description
+Add an optional extended description…
+ Commit directly to the main branch.
+ Create a new branch for this commit and start a pull request. Learn more about pull requests.
+ 
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
