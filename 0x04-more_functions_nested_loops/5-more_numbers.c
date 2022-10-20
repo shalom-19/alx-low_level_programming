0c186b1305fb57e14333256f9f4e7eaa6503e120
shalom-19 <113606094+shalom-19@users.noreply.github.com>
2022-10-20 16:22:48 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,12 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
 
 /**
- * more_numbers - Print numbers between 0 to 14 incl.
+ * main - check the code for Holberton School students.
  *
- * Return: Void.
+ * Return: Always 0.
  */
-void more_numbers(void)
+int main(void)
 {
-	int i;
-	int x;
-
-	for (x = 0; x < 10; x++)
-	{
-		i = 0;
-		while (i <= 14)
-		{
-			if (i >= 10)
-			{
-				_putchar(i / 10 + '0');
-			}
-				_putchar(i % 10 + '0');
-			i++;
-		}
-	_putchar('\n');
-	}
+    more_numbers();
+    return (0);
 }
