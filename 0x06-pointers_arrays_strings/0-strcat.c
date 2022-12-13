3d4f56cf046f66717b18ce0c09e7a88bf92340cb
shalom-19 <113606094+shalom-19@users.noreply.github.com>
2022-12-13 14:05:08 +0100

    array and string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - A function that concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: The pointer to the destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (j >= 0)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+		{
+			break;
+		}
+		i++;
+		j++;
+	}
+	return (dest);
+}
